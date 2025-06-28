440c9b02ec35e0a577fa310815531993e3a2357e
Moshe Zadka <moshez@zadka.club>
2025-06-28 02:05:43 +0000

    checkpoint

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,6 @@ master_doc = 'index'
 project = 'opinionated_mcp'
 copyright = 'Copyright (c) Moshe Zadka'
 author = 'Moshe Zadka'
-version = ''
+version = '1.0'
 
 exclude_patterns = ['.ipynb_checkpoints/**']
